cb6ebc2c8bf6113cae75120f16a4775068384f19
MohamedEL-gantery <elgantery50@gmail.com>
2023-07-04 07:07:02 +0300

    final

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -6,7 +6,7 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  if (!err.errmsg) {
+  if (!err || !err.errmsg) {
     return new AppError('Unknown error occurred', 500);
   }
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
