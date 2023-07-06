682caee95410cddb1ab3dd58c35f73ab80249e98
MohamedEL-gantery <elgantery50@gmail.com>
2023-07-06 07:31:34 +0300

    final

--- services/authGoogle.js
+++ services/authGoogle.js
@@ -15,43 +15,23 @@ passport.use(
       // 1) find if user exist with this email or not
       const user = await User.findOne({ email: profile.emails[0].value });
       if (!user) {
-        // 2) prompt user to create a password
-        const { prompt } = await import('inquirer');
-        const { password } = await prompt([
-          {
-            type: 'password',
-            name: 'password',
-            message: 'Enter your password:',
-            mask: '*',
-          },
-        ]);
-        const { confirmPassword } = await prompt([
-          {
-            type: 'password',
-            name: 'confirmPassword',
-            message: 'Confirm your password:',
-            mask: '*',
-          },
-        ]);
-
-        // 3) create new user with the password and confirmPassword fields set to the user's input
+        // 2) create new user
         const newuser = await User.create({
           email: profile.emails[0].value,
           name: profile.displayName,
           googleId: profile.id,
           photo: profile.photos[0].value,
-          password,
-          confirmPassword,
+          password: process.env.PASSWORD_GOOGLE,
+          confirmPassword: process.env.PASSWORD_GOOGLE,
           accessToken,
           refreshToken,
         });
-
-        // 4) If everything is ok, generate token
+        // 3) If everything is ok, generate token
         createSendToken(newuser, 201, request, request.res);
         console.log('user saved successfully to DB');
       } else {
         console.log('user already exists');
-        // 4) If everything is ok, generate token
+        // 3) If everything is ok, generate token
         createSendToken(user, 200, request, request.res);
       }
     }
